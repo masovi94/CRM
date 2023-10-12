4baf222986e8d3a032ef144ff697d862d36067ec
masovi94 <115945059+masovi94@users.noreply.github.com>
2023-10-12 12:19:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 // script.js (JavaScript para a Página de Login)
 
 function handleLogin() {
-    // Valide as credenciais do usuário aqui (envie para o servidor e valide)
-    var username = document.getElementById("mateus").value;
-    var password = document.getElementById("mateus").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-    // Simule uma validação bem-sucedida (você deve substituir isso pela lógica real)
-    if (username === "seu_usuario" && password === "sua_senha") {
-        // Redirecione para a página inicial após o login bem-sucedido
+    if (username === "mateus" && password === "mateus") {
+        // Redirecionar para a página inicial em caso de login bem-sucedido
         window.location.href = "home.html";
-        return false; // Evite o envio do formulário
     } else {
-        // Exiba uma mensagem de erro se o login falhar (você deve substituir isso pela lógica real)
-        document.getElementById("error").innerHTML = "Credenciais inválidas.";
-        return false;
+        document.getElementById("error").innerHTML = "Credenciais inválidas. Tente novamente.";
     }
+
+    // Impedir o envio do formulário
+    return false;
 }
